fd8d94141d4121890408553e30474580d1fc57b6
CjL95 <cjlong95@gmail.com>
2023-11-27 14:31:24 -0500

    testin

--- app/api/fetch_experience.tsx
+++ app/api/fetch_experience.tsx
@@ -1,2 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
+
+export default async function handler(req: any, res: any) {
+    const experience = await prisma.experience.findMany({});
+    res.status(200).json(experience);
+}
